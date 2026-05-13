c5c64322b7340425a2efdf2e4deff4e401426440
Javier Woo <147010985+woojavie@users.noreply.github.com>
2026-05-13 10:08:09 -0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # use binary search to find the index of the inner List
+        # use binary search to find target in inner list 
+        l, r = 0, len(matrix)-1
+        while l <= r:
+            m = (l + r) // 2
+            if target < matrix[m][0]:
+                r = m - 1
+            elif target > matrix[m][-1]:
+                l = m + 1
+            else:
+                break
+        if not (l <= r):
+            return False
+        l0, r0 = 0, len(matrix[m]) - 1
+        while l0 <= r0:
+            m0 = (l0 + r0) // 2
+            if target < matrix[m][m0]:
+                r0 = m0 - 1
+            elif target > matrix[m][m0]:
+                l0 = m0 + 1
+            else:
+                return True
+        return False
